e3487a2236fad440e09817b9987430d98664c14c
Eduardo Rodríguez Galleguillos <erodriguez.cl@gmail.com>
2023-06-09 22:41:01 -0400

    jsf/FlashSecretKey for Cluster

---
+++ src/main/java/org/joinfaces/example/config/TomcatConfig.java
@@ -0,0 +1,40 @@
+package org.joinfaces.example.config;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.ContextEnvironment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Value("${jsf.flash-secret-key}")
+    private String jsfFlashSecretKey;
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatFactory() {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected TomcatWebServer getTomcatWebServer(org.apache.catalina.startup.Tomcat tomcat) {
+                tomcat.enableNaming();
+                return super.getTomcatWebServer(tomcat);
+            }
+
+            @Override
+            protected void postProcessContext(Context context) {
+
+                logger.info("Initializing jsf/FlashSecretKey in postProcessContext");
+                // adding <env-entry>
+                ContextEnvironment ce = new ContextEnvironment();
+                ce.setName("jsf/FlashSecretKey");
+                ce.setType(String.class.getName());
+                ce.setValue(jsfFlashSecretKey);
+                context.getNamingResources().addEnvironment(ce);
+            }
+        };
+    }
+
+}
