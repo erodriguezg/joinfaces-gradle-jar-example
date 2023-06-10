bc223306ee0be2756084e56985fb56a8822d2ae8
Eduardo Rodríguez Galleguillos <erodriguez.cl@gmail.com>
2023-06-10 17:21:11 -0400

    joinfaces update and virtual threads preview

--- src/main/java/org/joinfaces/example/config/TomcatConfig.java
+++ src/main/java/org/joinfaces/example/config/TomcatConfig.java
@@ -1,13 +1,20 @@
 package org.joinfaces.example.config;
 
+import java.util.concurrent.Executors;
+
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.descriptor.web.ContextEnvironment;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @Configuration
 public class TomcatConfig {
 
@@ -38,6 +45,18 @@ public class TomcatConfig {
                 context.getNamingResources().addEnvironment(ce);
             }
         };
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
+    }
+
+    @Bean
+    public AsyncTaskExecutor applicationTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
     }
 
 }
